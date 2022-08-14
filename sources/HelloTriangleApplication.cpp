8223be887817dab638b1dd0daed2ba1264b07c05
Dumble009 <daichi19990821@gmail.com>
2022-08-14 18:33:31 +0900

    feat: HelloTriangleApplicationクラスにGLFWを用いたウインドウ周りの初期化処理を追加

--- sources/HelloTriangleApplication.cpp
+++ sources/HelloTriangleApplication.cpp
@@ -2,10 +2,28 @@
 
 void HelloTriangleApplication::run()
 {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
 }
 
 void HelloTriangleApplication::initVulkan()
 {
+}
+
+void HelloTriangleApplication::initWindow()
+{
+    glfwInit();
+
+    // GLFWはOpenGLでの使用を想定されてる作られているので、OpenGLのコンテキストを作成しないように指示を出す必要がある
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // リサイズできるウインドウは取り扱いが厄介なので、今はリサイズは出来ないように指定する
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    // 3つめのパラメータはウインドウのタイトル、4つめは表示するモニタの指定、5つめはOpenGLで使用されるもの
+    window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "Vulkan", nullptr, nullptr);
 }
 
 void HelloTriangleApplication::mainLoop()
--- sources/HelloTriangleApplication.hpp
+++ sources/HelloTriangleApplication.hpp
@@ -1,5 +1,6 @@
 #pragma once
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 class HelloTriangleApplication
 {
@@ -7,7 +8,14 @@ public:
     void run();
 
 private:
-    void initVulkan();
+    // -----変数の宣言-----
+    const int DEFAULT_WINDOW_WIDTH = 800;  // ウインドウ幅の初期値
+    const int DEFAULT_WINDOW_HEIGHT = 600; // ウインドウ高さの初期値
+    GLFWwindow *window;                    // GLFWのウインドウハンドラ
+
+    // -----関数の宣言-----
+    void initVulkan(); // Vulkan関連の初期化を行う
+    void initWindow(); // GLFW関連の初期化を行う
     void mainLoop();
     void cleanup();
 };
