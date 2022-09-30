a05a1b18ee5f06fb283226d632d3006c59539118
Dumble009 <daichi19990821@gmail.com>
2022-09-30 11:48:46 +0900

    feat: モデルとテクスチャを読み込むためのパス情報を定数変数としてHelloTriangleApplicationクラスに追加。また、createTextureImageが読み込む先をそのパスに変更

--- sources/HelloTriangleApplication.hpp
+++ sources/HelloTriangleApplication.hpp
@@ -114,6 +114,10 @@ private:
     // -----変数の宣言-----
     const int DEFAULT_WINDOW_WIDTH = 800;  // ウインドウ幅の初期値
     const int DEFAULT_WINDOW_HEIGHT = 600; // ウインドウ高さの初期値
+
+    const std::string MODEL_PATH = "models/viking_room.obj";
+    const std::string TEXTURE_PATH = "textures/viking_room.png";
+
     const int MAX_FRAMES_IN_FLIGHT = 2;
 
     const std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};   // 使用するvalidation layerの種類を指定
